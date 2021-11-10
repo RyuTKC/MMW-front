1daa1110800144ea318862b6de337b52feb658ee
RyuTKC <ddukes.tk@gmail.com>
2021-11-10 18:12:27 +0900

    start machinemanageweb front

--- src/jsx/components/CompoA.jsx
+++ src/jsx/components/CompoA.jsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, {useState} from "react";
 
 export default function CompoA(props) {
+  const [nickName, setNickName] = useState("パロパロ")
+  console.log(nickName);
+
   return (
     <>
-      <div className="reactApp">Aなんだよ</div>
       <h1>{props.arg1}野郎が</h1>
+      <input type=""></input>
+      <button onClick={()=> setNickName("うんこ")}>小学生の発想ボタン</button>
     </>
   );
 }
