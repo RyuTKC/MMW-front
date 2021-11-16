342ebcbe1ced5bb1f5ee4d38416a2cce434ba756
RyuTKC <ddukes.tk@gmail.com>
2021-11-17 02:19:17 +0900

    add routes-component

--- src/jsx/MachineManageWeb.jsx
+++ src/jsx/MachineManageWeb.jsx
@@ -4,6 +4,9 @@ import "../style/ReactTest.scss";
 import InputSetFunctional from "./components/InputSetFunctional";
 import InputSetClass from "./components/InputSetClass";
 import ReactTest from "./components/ReactTest";
+import { BrowserRouter as Router, Switch, Routes, Route, Link } from "react-router-dom"
+import Tinko from "./routes/tinko";
+import Unko from "./routes/unko";
 // import "../../style/pages/ReactTest.scss";
 
 const target = document.getElementById("app");
@@ -12,6 +15,14 @@ ReactDOM.render(
   <React.StrictMode>
     <InputSetFunctional />
     <InputSetClass />
+    <Router>
+      <Link to="/tinko"> go to tinko</Link>
+      <Link to="/unko"> go to unko</Link>
+      <Routes>
+        <Route path="tinko" element={<Tinko />} />
+        <Route path ="unko" element={<Unko />}　/>
+      </Routes>
+    </Router>
   </React.StrictMode>,
   target
 );
