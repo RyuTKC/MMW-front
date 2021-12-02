724c652a78ffbf0a9887926d518582543c66e796
RyuTKC <ddukes.tk@gmail.com>
2021-12-03 00:09:13 +0900

    edited

--- src/js/MachineManageWeb.tsx
+++ src/js/MachineManageWeb.tsx
@@ -5,7 +5,7 @@ import { BrowserRouter as Router, Link } from "react-router-dom" /* v5 */
 import { Header, Footer, Nav } from "components/_Commons/commonSet";
 import Routing from "js/pages/_RoutingV5";
 
-const target = document.getElementById("body");
+const target = document.getElementById("app");
 ReactDOM.render(
   <React.StrictMode>
     <Router>
--- src/js/pages/Machines.tsx
+++ src/js/pages/Machines.tsx
@@ -1,21 +1,22 @@
-import React, { VFC } from "react";
+import React, { VFC, useEffect } from "react";
 import { appConfig, MachinesAPI } from "appConfig";
 import { Link } from "react-router-dom";
 import axios from "axios";
 
 export default () => {
-    const getMachines = () => {
-        const res = appConfig.axios.get(MachinesAPI.root)
-            .then((res) => console.log(res));
+  const getMachines = () => {
+    const res = appConfig.axios.get<JSON>(MachinesAPI.root)
+      .then((res) =>
+        console.log(res.data, typeof res.data));
+  }
 
-            return res;
-    }
+  // リロード更新
+  useEffect(getMachines, [])
 
-    return (
-        // <main onLoad={getMachines()}>
-        <main>
-            <h2>I am Machines</h2>
-            <button onClick={getMachines}>now</button>
-        </main>
-    );
+  return (
+    <main>
+      <h2>I am Machines</h2>
+      <button onClick={getMachines}>now</button>
+    </main>
+  );
 };
