f20a43d403c300e1d2bf9ac292d777bcb8449bb0
RyuTKC <ddukes.tk@gmail.com>
2021-12-06 03:20:49 +0900

    editing machine-disp-module

--- src/js/pages/Machines.tsx
+++ src/js/pages/Machines.tsx
@@ -3,13 +3,29 @@ import { appConfig, MachinesAPI } from "appConfig";
 import { Link } from "react-router-dom";
 import axios from "axios";
 
-interface typeA{
-  object: object
+interface typeA {
+  administrator: string
+  assurance: string
+  created_at: string
+  host_name: string
+  machine_id: number
+  machine_name: string
+  maintenance_date: string
+  notes: string
+  place: string
+  product_id: number
+  purchase_date: string
+  qr_or_barcode: string
+  role_id: number
+  serial_number: string
+  status_type: number
+  updated_at: string
+  vender_id: number
 }
 
 export default () => {
   const getMachines = (): void => {
-    appConfig.axios.get(MachinesAPI.root)
+    appConfig.axios.get<typeA[]>(MachinesAPI.root)
       .then(res => {
         setJasonValue(res.data)
         console.log(res.data)
@@ -19,7 +35,7 @@ export default () => {
       })
   }
 
-  const [jsonValue, setJasonValue] = useState();
+  const [jsonValue, setJasonValue] = useState<typeA[]>([]);
   // リロード更新
   useEffect(getMachines, [])
 
@@ -27,12 +43,23 @@ export default () => {
     <main>
       <h2>I am Machines</h2>
       <button onClick={getMachines}>now</button>
-      <div>{JSON.stringify(jsonValue)}</div>
-      {/* <ul>
-        {jsonValue.map((item, index) => {
-          <li>{item}</li>
-        })}
-      </ul> */}
+      <table>
+        <thead></thead>
+        <tbody>
+          {jsonValue.map((value) => {
+            return (
+              <tr>
+                <td>{value.machine_id}</td>
+                <td>{value.machine_name}</td>
+                <td>{value.host_name}</td>
+                <td>{value.administrator}</td>
+                <td>{value.place}</td>
+                <td>{value.product_id}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
     </main>
   );
 };
